96b8cf042a67ff939b37866052f1c5da1446eee2
wningyua <55090146+wningyua@users.noreply.github.com>
2021-02-21 12:03:46 -0500

    Add files via upload

---
+++ Labs/503lab2:14notes.R
@@ -0,0 +1,16 @@
+# 503)lab_2/14
+
+# cv.err: always using the first one (unadjusted )
+
+
+
+# Data normalization is very crucial in KNN method.
+
+# Kfold_CV_knn: arguments - k-folds, k_in_KNN, label_in_training
+
+# cv_error: see pic
+
+# natural spline = cubic (k_knots + 4 df(flexible parameters)) +2 restrictors (at the begining and the end should be linear, with difference = 2 at each side )
+# 
+
+# smoothing spline: not we specify df rather than lambda
